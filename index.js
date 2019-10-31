673568f308671185e876d051f13e2d3284603d28
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-31 13:26:00 -0400

    defines dynamic port

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ server.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello Web23'})
 })
 
-server.listen(4000, () => console.log('\n === Server listening on port 4000 === \n'))
+const port = process.env.PORT || 4000;
+// port will be set by heroku after deployment 
+
+
+server.listen(port, () => console.log(`\n === Server listening on port ${port} === \n`))
